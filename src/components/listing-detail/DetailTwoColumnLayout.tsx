eb8cfcd9b141b513bbb24a7b6a35888dc30b77a9
LuanDHV <luandhv1406@gmail.com>
2026-06-25 15:31:36 +0700

    update ui

--- src/components/listing-detail/DetailTwoColumnLayout.tsx
+++ src/components/listing-detail/DetailTwoColumnLayout.tsx
@@ -10,9 +10,9 @@ export default function DetailTwoColumnLayout({
   aside,
 }: DetailTwoColumnLayoutProps) {
   return (
-    <div className="grid gap-5 lg:grid-cols-2 lg:items-stretch">
-      <div className="min-w-0 h-full">{main}</div>
-      <div className="min-w-0 h-full">{aside}</div>
+    <div className="grid gap-5 lg:grid-cols-3 lg:items-stretch">
+      <div className="min-w-0 h-full lg:col-span-2">{main}</div>
+      <div className="min-w-0 h-full lg:col-span-1">{aside}</div>
     </div>
   );
 }
