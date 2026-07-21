bc695e655a1254a5d7c594f7bc2afef08f5af22e
LuanDHV <luandhv1406@gmail.com>
2026-07-21 15:31:58 +0700

    update meta seo

--- apps/client/src/lib/site-config.ts
+++ apps/client/src/lib/site-config.ts
@@ -3,7 +3,7 @@ export const siteConfig = {
   url: "https://thuematbang.com.vn",
   googleTagManagerId: "GTM-TQQ8SV9H",
   description:
-    "Thuematbang.com.vn - Sàn kết nối bất động sản cho thuê hàng đầu Việt Nam, nơi tìm tin cho thuê, nhu cầu cần thuê, dự án và tin tức mới nhất để bạn kết nối nhanh hơn, đúng hơn và hiệu quả hơn.",
+    "Thuematbang.com.vn - Nền tảng kết nổi bất động sản hàng đầu, nhanh chóng, chính xác và tối ưu hóa hiệu quả mọi nhu cầu thuê và cho thuê của bạn.",
   defaultImage:
     "https://res.cloudinary.com/dhaccjb7x/image/upload/v1782097027/logo-TMB-white_pwwgf3.png",
 };
